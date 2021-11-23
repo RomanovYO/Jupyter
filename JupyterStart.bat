2bb7cc97cd3c50eda51b5ed2ef9d1ffb8c668a3b
RomanovYO <94523600+RomanovYO@users.noreply.github.com>
2021-11-24 00:09:28 +0200

    Добавлены переменные proxy

--- JupyterStart.bat
+++ JupyterStart.bat
@@ -1,4 +1,9 @@
 @echo off
+
+set https_proxy=http://172.16.1.30:3128
+set http_proxy=http://172.16.1.30:3128
+set ftp_proxy=http://172.16.1.30:3128
+
 c:
 cd C:\Work\DataAnalyst\Jupyter\
 jupyter notebook
