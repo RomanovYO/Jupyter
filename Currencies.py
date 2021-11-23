f7cb428fdc61ae20b72ae9f2442f5edac65acbc7
RomanovYO <94523600+RomanovYO@users.noreply.github.com>
2021-11-23 19:23:49 +0200

    Update Currencies.py

--- Currencies.py
+++ Currencies.py
@@ -3,7 +3,7 @@ import xmltodict
 
 url = 'http://www.cbr.ru/scripts/XML_daily.asp'
 responce = requests.get(url)
-# ~ print(responce.content)
+print(responce)
 data = xmltodict.parse(responce.content)
 # ~ print(data)
 
@@ -16,5 +16,4 @@ my_array = []
 for item in data['ValCurs']['Valute']:
   my_set = [item['CharCode'], item['NumCode'], item['Nominal'].rjust(5), item['Value'], item['Name']]
   my_array.append(my_set)
-  print(sorted(my_set))
-# ~ print(my_array)
+  print(my_set)
